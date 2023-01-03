15679cfc38e488426e91f23ec1314a5e80aa4b7e
jhonnycave <luis.j.cueva@gmail.com>
2023-01-03 16:43:40 -0500

    ok

--- src/Test/Page/LogoutPage.jsx
+++ src/Test/Page/LogoutPage.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { useAuth } from "../provider/auth";
 
 const LogoutPage = () => {
-    return <div></div>;
+    const values = useAuth();
+    const login = (e) => {
+        e.preventDefault();
+        values.logout();
+    };
+
+    console.log("values : ", values);
+
+    return (
+        <React.Fragment>
+            <h1>Logout</h1>
+            <form onSubmit={login}>
+                <label>Desea salir?</label>
+                <br />
+
+                <button type="submit">Salir</button>
+            </form>
+        </React.Fragment>
+    );
 };
 
 export default LogoutPage;
--- src/Test/Page/ProfilePage.jsx
+++ src/Test/Page/ProfilePage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../provider/auth";
 
 const ProfilePage = () => {
-    return <div></div>;
+    const values = useAuth();
+    if (!values.user) {
+        return <Navigate to="/login" />;
+    }
+
+    return (
+        <div>
+            <h1>Profile</h1>
+            <p>Wellcome {values.user?.username}</p>
+        </div>
+    );
 };
 
 export default ProfilePage;
