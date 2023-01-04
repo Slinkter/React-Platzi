cb268d1814694f0aeae11e7b45416fd6a5bf2edc
jhonnycave <luis.j.cueva@gmail.com>
2023-01-04 16:23:16 -0500

    configurando

---
+++ src/Basico/04.Navegacion-con-react-router/parte02/routes/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { EditTodoPage } from "./Edit/EditTodoPage";
+import { HomePage } from "./Home/HomePage";
+import { NewTodoPage } from "./New/NewTodoPage";
+
+function App() {
+    return (
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/new" element={<NewTodoPage />} />
+                <Route path="/edit/:id" element={<EditTodoPage />} />
+            </Routes>
+        </HashRouter>
+    );
+}
+
+export default App;
