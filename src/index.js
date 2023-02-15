5dc8ddeab3bac2a478c8702a20858c0122a4db4e
jhonnycave <luis.j.cueva@gmail.com>
2023-02-15 10:55:24 -0500

    save laptop

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./Profesional/04.Curso-de-unitTesting-jest/app/index";
+import App from "./Profesional/02.Curso-state-machines/App/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(App);
+root.render(<App />);
 
 // Profesional -04.Curso de unitesting-jest
 // root.render(App);
