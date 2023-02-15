b60796c3aaa349421197fc768bc37fb3f5f6859f
jhonnycave <luis.j.cueva@gmail.com>
2023-02-15 09:27:10 -0500

    revisando codigo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./Profesional/04.Curso-de-unitTesting-jest/app/index";
+import App from "./Basico/01.Curso-de-Introduccion-react/App/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(App);
+root.render(<App />);
 
 // Profesional -04.Curso de unitesting-jest
 // root.render(App);
