28b7802028a43ab0816dd6ff6bd575f454b397cc
slinkter <luis.j.cueva@gmail.com>
2023-05-30 09:46:13 -0500

    wind 11 : update rutas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./Basico/04.Navegacion-con-react-router/parte01/index";
+import App from "./02.Avanzado/01.Patrones-render-y-composicion/App/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
