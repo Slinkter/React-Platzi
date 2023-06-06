93065a0e539adc65a8340f8998f4239a0638722f
jhonnycave <luis.j.cueva@gmail.com>
2023-06-06 18:20:44 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./02.Avanzado/01.Patrones-render-y-composicion/App/index";
+import App from "./02.Avanzado/01.Patron-de-composicion/App/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
