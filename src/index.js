c850868285f03339db3692cf4d08c3df108c27b8
slinkter <luis.j.cueva@gmail.com>
2023-06-13 11:32:22 -0500

    win11 : post terminal ohmyposh

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./02.Avanzado/03.Navegacion-con-react-router/parte01/index";
+import App from "./02.Avanzado/03.Navegacion-con-react-router/parte02/routes/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
